79f3ddde3fc425533d235bccb0a5faa5923ff961
PomboObeso <matheus.gomes201@gmail.com>
2023-06-08 16:18:31 -0300

    commit inicial

---
+++ src/components/Banner/index.js
@@ -0,0 +1,30 @@
+import styles from './Banner.module.css';
+import circuloColorido from 'assets/circulo_colorido.png';
+import minhaFoto from 'assets/minha_foto.png';
+
+export default function Banner() {
+    return (
+        <div className={styles.banner}>
+            <div className={styles.apresentacao}>
+                <h1 className={styles.titulo}>
+                    Olá, Mundo!
+                </h1>
+                <p className={styles.paragrafo}>
+                Boas vindas ao meu espaço pessoal! Eu sou Matheus Oliveira, aluno de Front-end da Alura. Neste portifólio compartilho vários conhecimentos adquiridos, espero que goste :)
+                </p>
+            </div>
+            <div className={styles.imagens}>
+                <img 
+                    className={styles.circuloColorido}
+                    src={circuloColorido}
+                    aria-hidden={true} 
+                />
+                <img
+                    className={styles.minhaFoto}
+                    src={minhaFoto}
+                    alt="Foto do autor Mathues"
+                />
+            </div>
+        </div>
+    );
+}
---
+++ src/components/MenuLink/index.js
@@ -0,0 +1,15 @@
+import { NavLink } from 'react-router-dom';
+import styles from './MenuLink.module.css';
+
+export default function MenuLink({children, to}) {
+    return (
+        <NavLink 
+            to={to} 
+            className={({ isActive }) => `
+                ${styles.link}
+                ${isActive ? styles.link__ativo : ""}
+            `}>
+            {children}
+        </NavLink>
+    );
+}
